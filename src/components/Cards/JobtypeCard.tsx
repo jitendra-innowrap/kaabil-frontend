eeb6c26ee568b435d8adfce615b493498411b27f
jitendra <jitendra@innowrap.com>
2025-02-14 16:34:37 +0530

    Fixed: data update

--- src/components/Cards/JobtypeCard.tsx
+++ src/components/Cards/JobtypeCard.tsx
@@ -9,12 +9,12 @@ export default function JobtypeCard({icon, title, jobUrl}:jobcardtype) {
         <div className="flex items-center">
         <Image
             className="size-[60px] 2xl:size-[90px] cursor-pointer mx-auto"
-            src={'/new-assets/job-types/full-time.png'}
+            src={icon}
             width={90}
             height={90}
             alt="company-icons"
             />   
-        <h3 className='ml-4 font-medium text-xl 2xl:text-2xl '>Full time</h3>  
+        <h3 className='ml-4 font-medium text-xl 2xl:text-2xl '>{title}</h3>  
         </div>  
         <div className="bg-white size-10 2xl:size-14 rounded-full grid place-items-center">
             <MdArrowOutward />

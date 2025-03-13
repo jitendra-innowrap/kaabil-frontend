638d0410f8b0657bdd82f81f9a1c68254ed2cf20
jitendra <jitendra@innowrap.com>
2025-03-13 19:38:04 +0530

    Update: added suspense boundary for job listing apis

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -11,6 +11,7 @@ import QuickAction from "@/components/Nudges/Listing/QuickAction";
 import ResumeBuilder from "@/components/Nudges/Listing/ResumeBuilder";
 import SearchSection from "@/components/SearchSection";
 import Image from "next/image";
+import { Suspense } from "react";
 const jobsList = [
   {
    icon: "/new-assets/company-icons/image (1).png",
@@ -72,7 +73,9 @@ export default function Home() {
       <section className="container">
         <div className="mt-8 lg:mt-10 2xl:mt-14 mb-5 md:mb-8 xl:mb-14 2xl:mb-16 flex flex-col lg:flex-row gap-5 md:gap-7 xl:gap-10">
             <FilterSidebar/>
-            <JobList />
+            <Suspense fallback={<>... Loading</>}>
+              <JobList />
+            </Suspense>
             <div className="nudges-bar flex flex-shrink-0 flex-col gap-4 md:gap-6 max-w-[400px] mx-auto lg:w-[280px] 2xl:w-[341px]">
               <FindCareer/>
               <ProfileCard/>
